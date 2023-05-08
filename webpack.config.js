78332dade524a809a6af361fc8c795b7c56f86fd
Quaqva <sanek15056@gmail.com>
2023-05-08 14:56:58 +0400

    feat: load and configure dependencies

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const buildMode = process.env.NODE_ENV || 'development';
+const isDevMode = buildMode === 'development';
+const target = isDevMode ? 'web' : 'browserslist';
+const devtool = isDevMode ? 'source-map' : undefined;
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/script/index.js'],
+  mode: buildMode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'layout', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'post-css-loader',
+            options: {
+              postcssOptions: {
+                plugins: require('postcss-preset-env'),
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+};
